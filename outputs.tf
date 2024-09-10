65789a5b462a560809153cccb523deb16ada729e
Ritcher <ritcherstpreux@gmail.com>
2024-09-10 16:27:58 -0400

    1

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bastion_private_key" {
   value     = tls_private_key.bastion_key.private_key_pem
   sensitive = true
 }
+
+output "bastion_key_pair" {
+  value     = aws_key_pair.bastion_key
+  sensitive = true
+}
